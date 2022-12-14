c395068c1e1002b679b50dc3b25dc0a92800f645
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2022-12-14 14:57:49 +0530

    Time: 1 ms (86.65%), Space: 40 MB (95.45%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,16 +1,20 @@
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+
 class Solution {
     public String[] findWords(String[] words) {
         ArrayList<String> str = new ArrayList<>();
-        int idx = 0;
+
+        // Use a regular expression that matches all three patterns at once
+        Pattern p = Pattern.compile("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*");
 
         for (String word : words) {
             String s = word.toLowerCase();
-            if (s.matches("[qwertyuiop]*") || s.matches("[asdfghjkl]*")
-			  || s.matches("[zxcvbnm]*")) {
+            if (p.matcher(s).matches()) {
                 str.add(word);
             }
-
         }
+
         String res[] = str.toArray(new String[str.size()]); 
         return res;
     }
