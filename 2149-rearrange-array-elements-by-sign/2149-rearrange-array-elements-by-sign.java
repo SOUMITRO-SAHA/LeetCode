ad2f2c32dc88170f5353c98c401c65dd700d62ff
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2022-12-31 16:22:45 +0530

    Time: 5 ms (85.32%), Space: 79.6 MB (83.58%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        // Time-Complexity: O(n);
+        // Space-Complexity: O(n);
+        int len = nums.length;
+        int[] ans = new int[len];
+        int positive = 0;
+        int negative = 1;
+        
+        for(int i=0; i<len; i++){
+            if(nums[i] >= 0){
+                ans[positive] = nums[i];
+                positive += 2;
+            }
+            else if(nums[i] < 0){
+                ans[negative] = nums[i];
+                negative += 2;
+            }
+        }
+        
+        return ans;
+    }
+}
