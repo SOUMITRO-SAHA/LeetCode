9115fe3adb246f993eec511040cff7efc445d39d
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-03-04 10:35:39 +0530

    Time: 4 ms (84.72%), Space: 63.3 MB (24.06%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -43,9 +43,14 @@ class Solution {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode reversedList = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
-        return reversedList;
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
     }
 }
