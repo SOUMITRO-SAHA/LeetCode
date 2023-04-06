f32a87ab2b68e2e0ddde1557c60ade7926c01967
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-04-06 18:45:32 +0530

    Time: 1 ms (80.91%), Space: 42.9 MB (30.67%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,29 +14,37 @@
  * }
  */
 class Solution {
+    // This the BFS Algorithm: [Breath First Search]
+    // Most Important Algorithm
     public List<List<Integer>> levelOrder(TreeNode root) {
         // Edge Case:
         if(root == null){
             return new ArrayList<>();
         }
+        // First, Creating a Queue to Store the Queue Answer:
         Queue<TreeNode> q = new LinkedList<>();
         List<List<Integer>> ans = new ArrayList<>();
+        // Adding the Root node into the Queue:
         q.add(root);
-        int level = 0;
+        // When the Size of the Queue Will become zero then it will stop:
         while(q.size() > 0){
+            // Getting the size of the Queue:
             int size = q.size();
             List<Integer> currList = new ArrayList<>();
+            // It will run till the size become 0
             while(size-- > 0){
+                // Getting the Top Node in the Queue:
                 TreeNode rnode = q.remove();
                 currList.add(rnode.val);
+                // If the Current node has any Left node then add it to the Queue:
                 if(rnode.left != null){
                     q.add(rnode.left);
                 }
+                // If the Current node has any Right node then add it to the Queue:
                 if(rnode.right != null){
                     q.add(rnode.right);
                 }
             }
-            level++;
             ans.add(currList);
         }
         return ans;
