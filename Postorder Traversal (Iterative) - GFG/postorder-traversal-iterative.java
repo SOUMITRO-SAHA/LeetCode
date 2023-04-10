ff2311da28b0f3c22fb943cc25c173911ec5fdd2
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-04-10 16:29:35 +0530

    Added solution - LeetHub

---
+++ Postorder Traversal (Iterative) - GFG/postorder-traversal-iterative.java
@@ -0,0 +1,183 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Tree g = new Tree();
+            ArrayList<Integer> res = g.postOrder(root);
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.print("\n");
+            t--;
+        }
+    }
+}
+
+// Position this line where user code will be pasted.
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Tree {
+    // Return a list containing the inorder traversal of the given tree
+    /*
+    Post-Order => Left+Right+Root
+    Here, We are going to use a Stack to mimic call Stack:
+    
+    call = 1: Left Sub-Tree.
+    call = 2: Right Sub-Tree.
+    call = 3: Remove from Stack.
+    **/
+    class Pair{
+        int call;
+        Node node;
+        
+        Pair(Node node, int call){
+            this.node = node;
+            this.call = call;
+        }
+    }
+    
+    // Iterative Approach:
+    ArrayList<Integer> postOrder(Node root){
+        ArrayList<Integer> ans = new ArrayList<>();
+        // Edge Case:
+        if(root == null)
+            return ans;
+            
+        // Call Stack:
+        Stack<Pair> st = new Stack<>();
+        st.push(new Pair(root, 1));
+        
+        while(st.size() != 0){
+            // Case-1: Left
+            // Case-2: Right
+            // Case-3: pop
+            Pair rpair = st.peek();
+            Node currNode = rpair.node;
+            int call = rpair.call;
+            
+            // Coditions:
+            if(call == 1){
+                // left call:
+                if(currNode.left != null){
+                    st.push(new Pair(currNode.left, 1));
+                }
+                
+                rpair.call = 2;
+            }
+            else if(call == 2){
+                // right call:
+                if(currNode.right != null){
+                    st.push(new Pair(currNode.right, 1));
+                }
+                
+                rpair.call = 3;
+                
+            }
+            else if(call == 3){
+                // Adding the current node value to the Ans:
+                ans.add(currNode.data);
+                
+                st.pop();
+            }
+        }
+        
+        return ans;
+    }
+}
