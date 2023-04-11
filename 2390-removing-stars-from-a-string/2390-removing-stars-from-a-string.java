7cce4be0d415150b62125685da1ac9380ba83225
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-04-11 22:56:43 +0530

    Time: 49 ms (77.91%), Space: 48.1 MB (12.69%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,20 +1,24 @@
 class Solution {
+    // TC: O(n), SC:O(n)
     public String removeStars(String s) {
-        Stack<Character> st = new Stack<>();
+        // Implementing a Stack Using Deque:
+        Deque<Character> st = new LinkedList<>();
+        
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             if(ch == '*'){
-                if(st.size() > 0)st.pop();
+                if(st.size() > 0)st.removeLast();
                 continue;
             }
-            st.push(ch);
+            st.addLast(ch);
         }
         
         StringBuilder sb = new StringBuilder();
+        
         while(st.size() > 0){
-            sb.append(st.pop());
+            sb.append(st.removeFirst());
         }
         
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
