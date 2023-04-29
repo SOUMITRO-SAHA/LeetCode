b52cf670526db60fa2f8844491324cc1daafb88b
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-04-29 19:24:16 +0530

    Time: 3 ms (72.46%), Space: 43.4 MB (14.78%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -1,37 +1,16 @@
 class Solution {
-    // TC: O(N+M), SC: O(N)
     public List<String> removeAnagrams(String[] words) {
-        List<String> res = new ArrayList<>();
-        // TC: O(N) => Length of the Words array:
-        String prevCode = "";
-        for(String w: words){
-            String code = getCode(w); // O(M)
-            
-            if(!code.equals(prevCode)){
-                res.add(w);
+        String prev ="";
+        List<String> li=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            char[] ch=words[i].toCharArray();
+            Arrays.sort(ch);
+            String curr=String.valueOf(ch);
+            if(!curr.equals(prev)){
+                li.add(words[i]);
+                prev=curr;
             }
-            
-            prevCode = code;
         }
-        
-        return res;
-    }
-    
-    // TC: O(M) => M length of the word:
-    // SC: O(M)
-    private String getCode(String s){
-        Map<Character, Integer> map = new TreeMap<>();
-        
-        for(char ch: s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch,0)+1);
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for(char ch: map.keySet()){
-            sb.append(ch);
-            sb.append(map.get(ch));
-        }
-        
-        return sb.toString();
+        return li;
     }
 }
