ef35629ab808a4c0b228ae2f4ef44b8279c6b021
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-05-05 16:25:25 +0530

    Time: 127 ms (6.42%), Space: 52.2 MB (12.52%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+    // Base Case:
+    if(root == null) return new TreeNode(val);
+    
+    // Recursive Call:
+    if(val < root.val){
+        // Go Left:
+        root.left = insertIntoBST(root.left, val);
+    }
+    else{
+        // Go Right:
+        root.right = insertIntoBST(root.right, val);
+    }
+    
+    return root;
+};
