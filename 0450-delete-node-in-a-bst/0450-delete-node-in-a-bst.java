d2519cf36e47d89d7865ee9111cb401e725726a5
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-05-05 17:36:41 +0530

    Time: 0 ms (100.00%), Space: 43 MB (69.87%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,39 +14,48 @@
  * }
  */
 class Solution {
-    private int min(TreeNode root){
-        TreeNode temp = root;
-        while(temp.left != null) temp = temp.left;
-        return temp.val;
-    }
     public TreeNode deleteNode(TreeNode root, int key) {
-        // Base case:
+        // Base Case:
         if(root == null) return null;
-        
-        // Recursive Call:
-        // First, We have to find the Position:
+        // When We found the Key:
         if(root.val == key){
+            
             // Leaf Node:
-            if(root.left == null && root.right == null)return null;
-            // Only left Child:
-            else if(root.left != null && root.right == null) return root.left;
-            // Only having Right Child:
-            else if(root.right != null && root.left == null) return root.right;
+            if(root.left == null && root.right == null && root.val == key) return null;
 
-            // Having both the Child:
+            // Only Having Left Child:
+            else if(root.right == null && root.left != null && root.val == key) return root.left;
+            // Only Having Right Child:
+            else if(root.right != null && root.left == null && root.val == key) return root.right;
+
+            // Having Both Child:
             else{
-                // In this case I am gonna find the min element form right child:
-                int minEl = min(root.right);
-                root.val = minEl;
-                // Now, Delete the Duplicate Node form right child:
-                root.right = deleteNode(root.right, minEl);
-                return root;
+                // Find the Right Min Element:
+                int minR = min(root.right);
+
+                // Replace the target Node value with min value:
+                root.val = minR;
+
+                // Deleting the min:
+                root.right = deleteNode(root.right, minR);
             }
             
+            return root;
         }
-        else if(root.val < key) root.right = deleteNode(root.right, key);
-        else root.left = deleteNode(root.left, key);
+        else if(key < root.val){
+            // Go Left:
+            root.left = deleteNode(root.left, key);
+        }
+        else {
+            // Go Right:
+            root.right = deleteNode(root.right, key);
+        }
         
         return root;
     }
+    
+    private int min(TreeNode root){
+			// Base Case + Recursive Call:
+			return (root.left == null)? root.val : min(root.left);
+		}
 }
