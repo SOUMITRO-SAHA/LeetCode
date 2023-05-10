71422e3fd3e33ffb034ad3f907ff9952a58a945d
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-05-10 15:08:43 +0530

    Time: 5 ms (90.69%), Space: 43.2 MB (39.68%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,30 +1,33 @@
 class Solution {
-    // Brute-Force: Using Deque:
-    // TC: O(n): SC: O(n)
+    // Brute-Force Solution is in Notes:
+    // Optimized Approach: Using Two Pointers:
+    // TC: O(n); SC: O(1)
     public int minimumLength(String s) {
-        // Using Deque:
-		Deque<Character> dq = new ArrayDeque<>();
-
-		// Filling the dq with each characters:
-		for(char ch: s.toCharArray()){
-			dq.addLast(ch);
-		}
-
-		// Now, iterating till I found different Ended String:
-		while(dq.size() != 0){
-            if(dq.size() == 1) break;
-			// Codition-1: if first and last character are same:
-			char ch1 = dq.getFirst();
-			char ch2 = dq.getLast();
-			if(ch1 == ch2){
-				while(dq.size() != 0 && ch1 == dq.getFirst() ) dq.removeFirst();
-				while(dq.size() != 0 && ch2 == dq.getLast() ) dq.removeLast();
-			}
-
-			// Whenever the first char and last char are different break:
-			if(dq.size() > 0 && dq.getFirst() != dq.getLast()) break;
-		}
-
-		return dq.size();
+        int sp = 0;
+        int ep = s.length() -1;
+        
+        // Edge Case:
+        if(sp == ep) return 1;
+        
+        while(sp < ep){
+            // Checking whether first == last:
+            if(s.charAt(sp) != s.charAt(ep)) break;
+            
+            char ch = s.charAt(sp);
+            while(sp < ep && s.charAt(sp) == ch) sp++;
+            while(sp < ep && s.charAt(ep) == ch) ep--;
+            
+        }
+        
+        // Special Condition:
+        if(sp == ep){
+            if(sp != 0 && ep != s.length() -1){
+                // then Check for their neighbours:
+                char currChar = s.charAt(sp);
+                if(currChar != s.charAt(sp -1) && currChar != s.charAt(ep+1)) return 1;
+            }
+        }
+        
+        return (sp < ep)?ep - sp + 1: 0;
     }
 }
