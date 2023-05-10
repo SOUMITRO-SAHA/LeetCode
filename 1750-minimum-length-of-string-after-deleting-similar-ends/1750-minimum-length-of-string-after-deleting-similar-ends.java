57f8f4c0110ec0f34024bb919b474db7331494eb
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-05-10 14:44:27 +0530

    Time: 25 ms (6.88%), Space: 43.9 MB (5.67%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -0,0 +1,30 @@
+class Solution {
+    // Brute-Force: Using Deque:
+    // TC: O(n): SC: O(n)
+    public int minimumLength(String s) {
+        // Using Deque:
+		Deque<Character> dq = new ArrayDeque<>();
+
+		// Filling the dq with each characters:
+		for(char ch: s.toCharArray()){
+			dq.addLast(ch);
+		}
+
+		// Now, iterating till I found different Ended String:
+		while(dq.size() != 0){
+            if(dq.size() == 1) break;
+			// Codition-1: if first and last character are same:
+			char ch1 = dq.getFirst();
+			char ch2 = dq.getLast();
+			if(ch1 == ch2){
+				while(dq.size() != 0 && ch1 == dq.getFirst() ) dq.removeFirst();
+				while(dq.size() != 0 && ch2 == dq.getLast() ) dq.removeLast();
+			}
+
+			// Whenever the first char and last char are different break:
+			if(dq.size() > 0 && dq.getFirst() != dq.getLast()) break;
+		}
+
+		return dq.size();
+    }
+}
