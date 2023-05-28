b7c0a49a84c7c450e53f3b903f76a95bc2b8f97e
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-05-28 22:46:12 +0530

    Time: 1 ms (100.00%), Space: 59.5 MB (6.05%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,26 +1,19 @@
 class Solution {
+    // Optimum Approach:
+    // TC: O(n)
+    // SC: O(1)
     public int maxSubArray(int[] nums) {
         // Kadane's Algorithm:
         int max = Integer.MIN_VALUE;
         int sum = 0;
-        // Edge Case:
-        if(nums.length == 1){
-            return nums[0];
-        }
-        
-        // Finding the Maximum element in the array:
-        for(int el : nums) if(el > max) max = el;
         
         // Finding the Max subarray sum:
         for(int el : nums){
             sum += el;
+            max = Math.max(sum, max);
             if(sum < 0){
                 // -Ve Case Not required
                 sum = 0;
-            }
-            else{
-                // +Ve Case:
-                if(sum > max) max = sum;
             }
         }
 
