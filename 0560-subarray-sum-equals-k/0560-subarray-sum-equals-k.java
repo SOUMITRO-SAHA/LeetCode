4eb5f35fdfd71af846f1dae3a51029c832eb4089
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-06-03 19:47:39 +0530

    Time: 27 ms (58.26%), Space: 46.9 MB (45.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,18 +1,24 @@
 class Solution {
-    // Brute-Force Approach:
-    public int subarraySum(int[] nums, int x) {
-        int count = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                // Checking for sum == target value:
-                if(sum == x) count++;
+    // Optimal Approach:
+    // Using HashMap + Prefix Sum:
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> fmap = new HashMap<>();
+        fmap.put(0,1); // Filling value 0 with fq 1, before starting 
+        
+        int sum = 0, cnt = 0;
+        for(int el: nums){
+            sum += el;
+            int diff = sum - k;
+            // Check into the Map, Whether the difference present or not
+            // If present that means sub array with sum k present.
+            // update the fq of sum:
+            if(fmap.containsKey(diff)){
+                cnt += fmap.get(diff);
             }
             
+            // Updating the Frequency:
+            fmap.put(sum, fmap.getOrDefault(sum, 0)+1);
         }
-        
-        return count;
+        return cnt;
     }
 }
