a9809d229edc65cbbe05e073a25d53c648f07566
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-06-08 15:35:29 +0530

    Time: 2 ms (89.86%), Space: 40.4 MB (90.63%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,27 @@
 class Solution {
+    // I have to use Stack
     public boolean isValid(String s) {
+        // Edge Case:
+        if(s.length() == 1) return false;
+        
+        // Creating a Stack:
         Stack<Character> st = new Stack<>();
+        
+        // Putthing all the Opening paranthesis into the Stack:
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(ch == ')'){
-                if(st.size() == 0 || (st.size() > 0 && st.pop() != '('))
-                    return false;
-                continue;
+            if(ch == '(' || ch == '{' || ch == '['){
+                // Put into the Stack:
+                st.push(ch);
             }
-            else if(ch == '}'){
-                if(st.size() == 0 || (st.size() > 0 && st.pop() != '{'))
-                    return false;
-                continue;
-                
+            else{
+                if(st.size() > 0 && ch == ')' && st.peek() == '(') st.pop();
+                else if(st.size() > 0 && ch =='}' && st.peek() == '{') st.pop();
+                else if(st.size() > 0 && ch == ']' && st.peek() == '[') st.pop();
+                else return false;
             }
-            else if(ch == ']'){
-                if(st.size() == 0 || (st.size() > 0 && st.pop() != '['))
-                    return false;
-                continue;
-            }
-            st.push(ch);
         }
         
-        return st.size() == 0;
+        return st.isEmpty();
     }
 }
