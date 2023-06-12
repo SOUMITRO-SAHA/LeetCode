b46df07f76ff271297c092ceb8882f4f3b57521f
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-06-12 14:50:51 +0530

    Time: 156 ms (58.50%), Space: 55.4 MB (71.63%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,38 +1,43 @@
 class Solution {
-    public int largestRectangleArea(int[] arr) {
-        int n = arr.length;
-        int maxArea = 0;
+    // Optimised Approach:
+    // Using Stack:
+    public int largestRectangleArea(int[] heights) {
+        int area = 0;
+        int len = heights.length;
         
+        // Creating stack: Only stores the index;
         Stack<Integer> st = new Stack<>();
         
-        for(int i=0; i<n; i++){
-            int ele = arr[i];
-            while(st.size() > 0 && ele < arr[st.peek()]){
+        for(int i=0; i<len; i++){
+            // Get the element
+            int ele = heights[i];
+            
+            // Find the height greater then peek
+            while(st.size()>0 && ele < heights[st.peek()]){
                 int idx = st.pop();
                 int rb = i;
-                int lb = (st.size() > 0)?st.peek(): -1;
+                int lb = st.size() > 0? st.peek(): -1;
                 
-                int width = rb - lb -1;
-                int height = arr[idx];
+                int w = rb - lb -1;
+                int h = heights[idx];
+                int currArea = w * h;
                 
-                int currArea = width * height;
-                maxArea = Math.max(maxArea, currArea);        
+                area = Math.max(currArea, area);
             }
+            // Adding the height as potentia height
             st.push(i);
         }
-        
-        while(st.size() >0){
+        // If the Stack is still not emptied:
+        while(st.size() > 0){
             int idx = st.pop();
-            int rb = n;
-            int lb = (st.size() > 0)?st.peek(): -1;
+            int rb = len;
+            int lb = st.size() > 0? st.peek(): -1;
 
-            int width = rb - lb -1;
-            int height = arr[idx];
-
-            int currArea = width * height;
-            maxArea = Math.max(maxArea, currArea);
+            int w = rb - lb -1;
+            int h = heights[idx];
+            int currArea = w * h;
+            area = Math.max(currArea, area);
         }
-        
-        return maxArea;
+        return area;
     }
 }
