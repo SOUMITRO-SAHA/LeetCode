e4110529f623563d7edf3a726bdb9fcb5d4320c0
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-06-13 10:39:22 +0530

    Added solution - LeetHub

---
+++ Remove K Digits - GFG/remove-k-digits.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String S = sc.next();
+            int K = sc.nextInt();
+            Solution obj = new Solution();
+            System.out.println(obj.removeKdigits(S, K));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public String removeKdigits(String s, int k) {
+        if(k == s.length()) return "0";
+        
+        // Stack to store the digits
+        Stack<Character> st = new Stack<>();
+        int slen = s.length();
+        
+        for(int i=0; i<slen; i++){
+            // get element
+            char ch = s.charAt(i);
+            // check with the peek
+            while(k > 0 && st.size() > 0 && ch < st.peek()){
+                st.pop();
+                k--;
+            }
+            // push the current element into the stack
+            st.push(ch);
+        }
+        
+        // Remove remaining digits if k > 0
+        while(k > 0 && !st.isEmpty()){
+            st.pop();
+            k--;
+        }
+        
+        // Build the result string
+        StringBuilder sb = new StringBuilder();
+        
+        // Append digits from the stack in reverse order
+        while(st.size() > 0){
+            sb.append(st.pop());
+        }
+        
+        // Reversing the result string to get the correct order
+        sb.reverse();
+        
+        // Removing the leading zeros:
+        while(sb.toString().startsWith("0")){
+            sb.deleteCharAt(0);
+        }
+        
+        // If the result is empty, return '0', else return the result
+        return sb.length() == 0? "0": sb.toString();
+    }
+}
