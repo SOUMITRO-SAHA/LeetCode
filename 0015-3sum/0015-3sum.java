6e6bba5bcdedfb86cdbd38ed086eae4dc468afcb
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-06-13 14:09:28 +0530

    Time: 56 ms (48.49%), Space: 51.7 MB (12.98%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,47 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        // Step-1: Sort the Array:
+        // Step 1: Sort the array in non-decreasing order
         Arrays.sort(nums); // O(n log n)
 
         int i = 0;
         int n = nums.length;
-        while(i<n){
-            int l = i+1;
-            int r = nums.length -1;
-            int target = 0 - nums[i];
-            int prevI = nums[i];
-            while(l < r){
-                int prevR = nums[r];
-                int prevL = nums[l];
-                int sum = nums[l] + nums[r];
+        while (i < n) {
+            int left = i + 1;
+            int right = n - 1;
+            int target = 0 - nums[i]; // Calculate the target sum
+            int prevI = nums[i]; // Store the previous value of nums[i] for comparison
 
-                if(sum == target){
-                    List<Integer> ans = new ArrayList<>();
-                    ans.add(nums[i]); ans.add(nums[l]); ans.add(nums[r]);
+            while (left < right) {
+                int prevRight = nums[right];
+                int prevLeft = nums[left];
+                int sum = nums[left] + nums[right];
 
-                    res.add(ans);
-                    while(l < r && nums[l] == prevL) l++;
-                    while(r > l && nums[r] == prevR) r--;
+                if (sum == target) {
+                    // Found a valid triplet that sums up to the target
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[left]);
+                    triplet.add(nums[right]);
+                    result.add(triplet);
+
+                    // Move the left and right pointers to skip duplicates
+                    while (left < right && nums[left] == prevLeft) left++;
+                    while (left < right && nums[right] == prevRight) right--;
+                } else if (sum > target) {
+                    // Reduce the sum by moving the right pointer towards the left
+                    while (left < right && nums[right] == prevRight) right--;
+                } else {
+                    // Increase the sum by moving the left pointer towards the right
+                    while (left < right && nums[left] == prevLeft) left++;
                 }
-                else if(sum > target) while(r > l && nums[r] == prevR) r--;
-                else while(l < r && nums[l] == prevL) l++;
             }
-            while(i < n && nums[i] == prevI) i++;
+
+            // Move the i pointer to skip duplicates
+            while (i < n && nums[i] == prevI) i++;
         }
 
-        return res;
+        return result;
     }
 }
