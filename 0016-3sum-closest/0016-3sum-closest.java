f64e9a19d09a8232f998321de80458098d45b870
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-06-14 10:21:30 +0530

    Time: 15 ms (68.24%), Space: 43.3 MB (22.40%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,23 +1,42 @@
+import java.util.Arrays;
+
 class Solution {
+    // TC: O(n^2), SC: O(1)
     public int threeSumClosest(int[] nums, int target) {
+        // Initialize variables to store the minimum difference and the answer
         int diff = Integer.MAX_VALUE;
         int ans = 0;
+
+        // Sort the input array in ascending order
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
+
+        // Iterate through the array, considering each element as the first number in the triplet
+        for (int i = 0; i < nums.length - 2; i++) { // O(n)
+            // Set the low and high pointers for the remaining elements in the array
             int low = i + 1;
             int high = nums.length - 1;
+            
+            // Store the value of the first number in the triplet
             int first = nums[i];
-            while (low < high) {
+            
+            // Continue searching for the other two numbers until low and high pointers meet
+            while (low < high) { // O(n)
+                // Calculate the current sum and the absolute difference between the sum and the target
                 int currSum = first + nums[low] + nums[high];
-                int currDiff = Math.abs(first + nums[low] + nums[high] - target);
+                int currDiff = Math.abs(currSum - target);
                 
+                // If the current sum equals the target, return the target sum
                 if (currSum == target) {
                     return target;
-                } else if ( currDiff < diff) {
+                } 
+                // If the current difference is smaller than the minimum difference encountered so far,
+                // update the minimum difference and the answer
+                else if (currDiff < diff) {
                     diff = currDiff;
                     ans = currSum;
                 }
                 
+                // Adjust the low and high pointers based on the comparison with the target sum
                 if (currSum < target) {
                     low++;
                 } else {
@@ -25,6 +44,8 @@ class Solution {
                 }
             }
         }
+        
+        // Return the closest sum to the target
         return ans;
     }
 }
