09e9bacddb8242b31f8fd60a84b3754beb2e7e1f
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-06-22 15:58:59 +0530

    Time: 280 ms (5.09%), Space: 43 MB (68.18%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    // Brute-Force Approach:
+    // TC: O(n^2), SC: O(1)
+    public int maxProduct(int[] nums) {
+        int p = Integer.MIN_VALUE;
+        
+        for(int i = 0; i<nums.length; i++){
+            int currP = nums[i];
+            p = Math.max(p, currP);
+            for(int j=i+1; j<nums.length; j++){
+                currP *= nums[j];
+                p = Math.max(p, currP);
+            }
+            
+        }
+        return p;
+    }
+}
