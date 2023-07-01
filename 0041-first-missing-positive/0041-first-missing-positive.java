5b35aa03c43a8d5d3211d2d045e6849eb94d61f7
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-07-01 11:38:38 +0530

    Time: 13 ms (29.60%), Space: 62.3 MB (8.90%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        // Creating a HashMap:
+        Set<Integer> set = new HashSet<>();
+        
+        // Putting all the elements into the Array:
+        int max = 0;
+        for(int el: nums){
+            max = Math.max(max, el);
+            set.add(el);
+        } 
+        
+        // Iteration:
+        for(int i=1; i <= n+1; i++){
+            if(!set.contains(i)) return i;
+        }
+        
+        return -1;
+    }
+}
