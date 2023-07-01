ab66588f471d15a3b9874eb60917b014b8f55607
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-07-01 12:05:05 +0530

    Time: 2 ms (65.66%), Space: 59.7 MB (16.45%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,21 +2,32 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         
-        // Creating a HashMap:
-        Set<Integer> set = new HashSet<>();
-        
-        // Putting all the elements into the Array:
-        int max = 0;
-        for(int el: nums){
-            max = Math.max(max, el);
-            set.add(el);
-        } 
-        
-        // Iteration:
-        for(int i=1; i <= n+1; i++){
-            if(!set.contains(i)) return i;
+        boolean one = false;
+        // Making the elements which are out of range
+        for(int i=0; i<n; i++){
+            int ele = nums[i];
+            if(ele == 1) one = true;
+            if(ele > n || ele < 1) nums[i] = 1;
         }
         
-        return -1;
+        // Check whether one = false or not
+        // if false return 1
+        if(one == false) return 1;
+        
+        
+        // Checking for Missing Number:
+        for(int i=0; i<n; i++){
+            int idx = Math.abs(nums[i]);
+            
+            // making the relatinve index with -1
+            nums[idx -1] = -Math.abs(nums[idx -1]);
+        }
+        
+        // Now check for any positive value:
+        for(int i=0; i<n; i++){
+            if(nums[i] > 0) return i+1;
+        }
+        
+        return n + 1;
     }
 }
