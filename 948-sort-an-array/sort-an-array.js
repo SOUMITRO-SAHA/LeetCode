12b324a7102c56f462878ddf281345cf6cc25e57
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-12-31 21:05:14 +0530

    Time: 46 ms (91.62%) | Memory: 69.2 MB (83.33%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    // TODO: Solve this question using Merge Sort
+    return nums.sort((a, b) => a - b);
+};
